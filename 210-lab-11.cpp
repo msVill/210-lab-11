a7dc8cc44d0121f076cdd1e9842eab39c21b6d91
msVill <msv.ltmc@gmail.com>
2026-02-21 09:58:36 -0800

    Added: tracking/purpose comments

---
+++ 210-lab-11.cpp
@@ -0,0 +1,19 @@
+// comsc 210 | Martha Stephanie Villalta | Lab 11
+
+#include <iostream>
+using namespace std;
+
+// -> use struct
+//.  -> the struct definition will include a dynamic array (+ other members).
+// -> dynamic array of structs.
+// Before I begin: I want to make sure I follow my plan correctly:
+//  --> I want to create an assignment tracker because my current system is not working:
+// --> I need to the number of assignments due for each class , 
+// and maybe I need to let the program know HOW many classes to track.
+// --> I want to output the class names, and the number of assignments due.
+
+
+int main() {
+
+    return 0;
+}
