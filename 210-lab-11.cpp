8262f32d6f6d73a82801e08becb500520924abdc
msVill <msv.ltmc@gmail.com>
2026-02-22 09:19:19 -0800

    Added for-loop to iterate through/over the courses dynamic array

--- 210-lab-11.cpp
+++ 210-lab-11.cpp
@@ -28,5 +28,11 @@ int main() {
 
     // Okay, what do I want to do now?... I need to dynamically allocate array of Course structs
     Course* courses = new Course[numCourses];
+
+    // I want to be able to get that data and store it
+    for(int i = 0; i < numCourses; i++) {
+        cout << "Enter name for course #" << i + 1 << "; ";
+        cin >> courses[i].courseName;
+    }
     return 0;
 }
